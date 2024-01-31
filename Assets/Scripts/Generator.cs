33c2c4317a10b454e8d7914d348243912fa49ce7
Joldd <julien.coutouly@gmail.com>
2024-01-31 11:20:59 +0100

    btn generate

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Generator : MonoBehaviour
 {
     const int TRAIT_COUNT = 3;
-    
+
+    const int PNJ_COUNT = 6;
+
     [SerializeField] List<Categorie> _categories;
+
+    [SerializeField] GameObject card_PNJ;
+
+    [SerializeField] GameObject PNJ_Group;
 
     [ContextMenu("Generate")]
     public void Generate()
     {
+        for (var i = 0; i < PNJ_COUNT; i++)
+        {
+            Instantiate(card_PNJ, PNJ_Group.transform);
+        }
+
         var categoriesClone = new List<Categorie>(_categories);
         var traits = new List<Trait>();
         
