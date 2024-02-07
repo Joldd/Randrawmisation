29f7346860ffd7577444df76c302deae4c6d0737
Joldd <julien.coutouly@gmail.com>
2024-02-07 10:32:21 +0100

    link image traits

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,6 +11,8 @@ public class Character : MonoBehaviour
     [SerializeField] Image _hatImage;
     [SerializeField] Image _clothesImage;
     [SerializeField] Image _beardImage;
+    [SerializeField] Image _happyImage;
+    [SerializeField] Image _itemImage;
 
     [SerializeField] TextMeshProUGUI _nameText;
     [SerializeField] TextMeshProUGUI _traitsText;
@@ -20,9 +22,17 @@ public class Character : MonoBehaviour
     public int Age { get; set; }
     public int Sexe { get; set; }
     public Sprite IconFace { get; set; }
+    public bool faceSet = false;
     public Sprite IconHat { get; set; }
+    public bool hatSet = false;
     public Sprite IconClothes { get; set; }
+    public bool clothesSet = false;
     public Sprite IconBeard { get; set; }
+    public bool beardSet = false;
+    public Sprite IconHappy { get; set; }
+    public bool happySet = false;
+    public Sprite IconItem { get; set; }
+    public bool itemSet = false;
 
     public List<Trait> Traits { get; set; }
     public List<LocalizedString> Resume { get; set; }
@@ -35,6 +45,8 @@ public class Character : MonoBehaviour
         UpdateImage(_clothesImage, IconClothes);
         UpdateImage(_hatImage, IconHat);
         UpdateImage(_beardImage, IconBeard);
+        UpdateImage(_happyImage, IconHappy);
+        UpdateImage(_itemImage, IconItem);
 
         _traitsText.text = string.Join(" - ", Traits.Select(trait => trait.Name));
         _resumeText.text = string.Join("\n\n", Resume.Select(strings => strings.GetLocalizedString()));
